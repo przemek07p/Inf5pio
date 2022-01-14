a4c516717fa8d713652ffb36b5359cb8a3075a84
sobik.sebastian@gmail.com <sobik.sebastian@gmail.com>
2022-01-14 21:34:43 +0100

    little update

--- database/entities/Admin.cs
+++ database/entities/Admin.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace database.entities
 {
     public class Admin : IRoleBase
     {
+        [Key]
         public int RoleId { get; set; }
         public string Login { get; set; }
         public string Password { get; set; }
--- database/entities/User.cs
+++ database/entities/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
@@ -9,6 +10,7 @@ namespace database.entities
 {
     public class User : IRoleBase
     {
+        [Key]
         public int RoleId { get; set; }
         public string Login { get; set; }
         public string Password { get; set; }
