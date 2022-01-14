5b697b7b4bb692c6c1b4e47f1e7d53fb77799ee3
Artur Śleziński <as055959@student.ath.edu.pl>
2022-01-14 19:11:37 +0100

    Dodaj pliki projektów.

---
+++ BRANCH CAR MANAGER/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BRANCH_CAR_MANAGER
+{
+    public partial class BRANCH : Form
+    {
+        public BRANCH()
+        {
+            InitializeComponent();
+        }
+
+        private void BRANCH_CAR_MANAGER_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
