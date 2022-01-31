83c3229f8193d5d77e60d60592816f0485e6ee51
Artur Śleziński <as055959@student.ath.edu.pl>
2022-01-31 20:01:39 +0100

    unifying the colors of the menu and title bar

--- BRANCH CAR MANAGER/Form1.cs
+++ BRANCH CAR MANAGER/Form1.cs
@@ -52,7 +52,8 @@ namespace BRANCH_CAR_MANAGER
                     currentButton.BackColor = color;
                     currentButton.ForeColor = Color.White;
                     currentButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-                  
+                    panelTitleBar.BackColor = color;
+                    panelLogo.BackColor = Theme.ChangeColorBrightness(color, -0.3);
                 }
             }
         }
--- BRANCH CAR MANAGER/Theme.cs
+++ BRANCH CAR MANAGER/Theme.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -36,6 +37,29 @@ namespace BRANCH_CAR_MANAGER
                                                                     "#43B76E",
                                                                     "#7BCFE9",
                                                                     "#B71C46"};
-        
+
+        public static Color ChangeColorBrightness(Color color, double correctionFactor)
+        {
+            double red = color.R;
+            double green = color.G;
+            double blue = color.B;
+            
+            if (correctionFactor < 0)
+            {
+                correctionFactor = 1 + correctionFactor;
+                red *= correctionFactor;
+                green *= correctionFactor;
+                blue *= correctionFactor;
+            }
+            
+            else
+            {
+                red = (255 - red) * correctionFactor + red;
+                green = (255 - green) * correctionFactor + green;
+                blue = (255 - blue) * correctionFactor + blue;
+            }
+            return Color.FromArgb(color.A, (byte)red, (byte)green, (byte)blue);
+        }
+
     }
 }
