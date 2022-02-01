33ad21901a9644507914afd46384e3e7917ab6ef
Artur Śleziński <as055959@student.ath.edu.pl>
2022-02-01 21:10:28 +0100

    Unit Tests

---
+++ database/services/CarService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using database.repositories;
+
+namespace database.services
+{
+    public class CarService : ICarService
+    {
+        readonly IUnitOfWork _unitOfWork;
+
+        public CarService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public string GetVehicleId(int id)
+        {
+            var item = _unitOfWork.CarRepository.GetById(id);
+
+            if (item != null)
+            {
+                return item.RegistrationNumber;
+            }
+
+            throw new ArgumentException("Błędne ID");
+        }
+    }
+}
+
---
+++ databaseTests/UnitTests.cs
@@ -0,0 +1,60 @@
+﻿using database.entities;
+using database.repositories;
+using database.services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace databaseTests
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void CarServiceGivenIdReturnsRegistrationNumber()
+        {
+            //Arrange
+            var VehicleId = 1;
+            var expected = "WW54855";
+            var car = new Car() { RegistrationNumber = expected, VehicleId = VehicleId };
+
+            var carRepositoryMock = new Mock<IRepository<Car>>();
+            carRepositoryMock.Setup(m => m.GetById(VehicleId)).Returns(car).Verifiable();
+
+            var unitOfWorkMock = new Mock<IUnitOfWork>();
+            unitOfWorkMock.Setup(m => m.CarRepository).Returns(carRepositoryMock.Object);
+
+            ICarService sut = new CarService(unitOfWorkMock.Object);
+
+            //Act
+            var actual = sut.GetVehicleId(VehicleId);
+
+            //Assert
+            carRepositoryMock.Verify();
+            Assert.IsNotNull(actual);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CarServiceGivenIdReturnsException()
+        {
+            //Arrange
+            var VehicleId = 1;
+            var expected = "WW54855";
+            var car = new Car() { RegistrationNumber = "PO72512", VehicleId = 7 };
+
+            var carRepositoryMock = new Mock<IRepository<Car>>();
+            carRepositoryMock.Setup(m => m.GetById(VehicleId)).Returns(car).Verifiable();
+
+            var unitOfWorkMock = new Mock<IUnitOfWork>();
+            unitOfWorkMock.Setup(m => m.CarRepository).Returns(carRepositoryMock.Object);
+
+            ICarService sut = new CarService(unitOfWorkMock.Object);
+
+            //Act
+            var actual = sut.GetVehicleId(VehicleId);
+
+            //Assert
+            Assert.AreNotEqual(expected, actual);
+        }
+    }
+}
