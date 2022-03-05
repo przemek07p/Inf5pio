6ac06f8af3975bb7213d1e53131ac7bc3168bdb9
przemek07p <przemek07p@gmail.com>
2022-03-05 22:20:51 +0100

    Test1

--- databaseTests/UnitTests.cs
+++ databaseTests/UnitTests.cs
@@ -105,5 +105,29 @@ namespace databaseTests
             Assert.IsNotNull(actual);
             Assert.AreEqual(expected, actual);
         }
+        [TestMethod]
+        public void CarServiceGivenIdReturnsAge()
+        {
+            //Arrange
+            var VehicleId = 1;
+            var expected = "21";
+            var car = new Car() { RegistrationNumber = expected, VehicleId = VehicleId };
+
+            var carRepositoryMock = new Mock<IRepository<Car>>();
+            carRepositoryMock.Setup(m => m.GetById(VehicleId)).Returns(car).Verifiable();
+
+            var unitOfWorkMock = new Mock<IUnitOfWork>();
+            unitOfWorkMock.Setup(m => m.CarRepository).Returns(carRepositoryMock.Object);
+
+            ICarService sut = new CarService(unitOfWorkMock.Object);
+
+            //Act
+            var actual = sut.GetVehicleId(VehicleId);
+
+            //Assert
+            carRepositoryMock.Verify();
+            Assert.IsNotNull(actual);
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
